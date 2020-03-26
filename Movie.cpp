d43d0a36b9058226090b54235051ec3388705046
rohansamuel99 <r.hariharan@uea.ac.uk>
2020-03-26 15:53:55 +0000

    created setter and getter methods added a ntf

--- Movie.cpp
+++ Movie.cpp
@@ -3,8 +3,6 @@
 **/
 
 #include "Movie.h"
-#include <string>
-#include <iostream>
 
 
 // creat a movie object for each film
@@ -17,13 +15,77 @@
   6. User Rating maybe
 */
 
-class Movie
+// make an array of fixed size 6 and then once the file is read in assign array[0] to title and so on
+
+namespace movie
 {
+    class Movie
+    {
     public:
-        string filmTitle;
-        int releaseYear;
-        string ageRating;
-        string genre;
-        int lengthMinutes;
-        int viewerRating;
-};
+        //setter methods for title, release year, age rating, genre, length and viewer rating
+        //nts*-inline function is a function that is expanded in line when it is called, they are faster cos no push and pop on and off the stack
+        inline void setFilmTitle(std:: string& newFilmTitle)
+        {
+            this->filmTitle = newFilmTitle;
+        }
+
+        inline void setReleaseYear(int &newReleaseYear)
+        {
+            this->releaseYear = newReleaseYear;
+        }
+
+        inline void setAgeRating(std:: string& newAgeRating)
+        {
+            this->ageRating = newAgeRating;
+        }
+
+        inline void setGenre(std:: string& newGenre)
+        {
+            this->genre = newGenre;
+        }
+
+        inline void setLength(int &newLength)
+        {
+            this->length = newLength;
+        }
+
+        inline void setViewerRating(int &newViewerRating)
+        {
+            this->viewerRating = newViewerRating;
+        }
+
+        // getter mehtods for title, release year, age rating, genre, length and viewer rating
+        inline std:: string getFilmTitle()
+        {
+            return this->filmTitle;
+        }
+
+        inline int& getReleaseYear()
+        {
+            return this->releaseYear;
+        }
+
+        inline std:: string getAgeRating()
+        {
+            return this->ageRating;
+        }
+
+        inline std:: string getGenre()
+        {
+            return this->genre;
+        }
+
+        inline int& getLength()
+        {
+            return this->length;
+        }
+
+        inline int& getViewerRating()
+        {
+            return this->viewerRating;
+        }
+
+    };
+
+    //string[] filmsHelp = lines.split(csv);
+}
