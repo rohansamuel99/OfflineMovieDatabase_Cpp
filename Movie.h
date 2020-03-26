9305168345b5c7691bde2f5f3b807682a1851ce5
rohansamuel99 <r.hariharan@uea.ac.uk>
2020-03-26 15:54:22 +0000

    added a ntf and commented out string csv

--- Movie.h
+++ Movie.h
@@ -5,4 +5,23 @@
 #ifndef OFFLINEMOVIEDATABASE_CPP_MOVIE_H
 #define OFFLINEMOVIEDATABASE_CPP_MOVIE_H
 
+#include <string>
+#include <fstream>
+#include <iostream>
+
+struct Movie
+{
+public:
+    //ntf* unsigned is used to make the variable only represent natural numbers
+    std:: string filmTitle;
+    unsigned int releaseYear;
+    std:: string ageRating;
+    std:: string genre;
+    unsigned int length;
+    unsigned int viewerRating;
+    // string csv = ",";
+};
+
+
+
 #endif //OFFLINEMOVIEDATABASE_CPP_MOVIE_H
