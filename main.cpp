1580d1e06970b59e5ab8105e0ee4111d931baba3
rohansamuel99 <r.hariharan@uea.ac.uk>
2020-03-27 17:56:12 +0000

    reading in file and printing out its contents

--- main.cpp
+++ main.cpp
@@ -2,3 +2,18 @@
 // Created by rohan on 25/03/2020.
 //
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
+//read in the file and print out its contents
+int main()
+{
+    //read in the file and print out its contents
+    std::ifstream file("C:\\Users\\rohan\\CLionProjects\\OfflineMovieDatabase_Cpp\\films.txt");
+    std::string str;
+    while (std::getline(file, str)) {
+        std::cout << str << "\n";
+    }
+
+}
